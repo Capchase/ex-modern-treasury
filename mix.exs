ff4891b67c72a89a5386a7df9d31efc117da8779
Joan Albert Segura Rueda <jalbertsr@protonmail.ch>
2021-03-26 09:41:41 +0100

    chore(ci): add code analysis

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ModernTreasurer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -21,8 +22,18 @@ defmodule ModernTreasurer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+      ignore_warning: ".dialyzer_ignore.exs"
     ]
   end
 end
