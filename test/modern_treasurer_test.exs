eb2413b1fa723a61dd8e98bb47426032c793d8cc
Joan Albert Segura <jalbertsr@protonmail.ch>
2021-03-30 12:27:04 +0200

    feat(counterparty): get and create (#5)

--- test/modern_treasurer_test.exs
+++ test/modern_treasurer_test.exs
@@ -1,8 +1,20 @@
 defmodule ModernTreasurerTest do
-  use ExUnit.Case
-  doctest ModernTreasurer
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  test "greets the world" do
-    assert ModernTreasurer.hello() == :world
+  setup_all do
+    HTTPoison.start()
+  end
+
+  test "test get counterparty" do
+    ExVCR.Config.filter_request_headers("Authorization")
+
+    use_cassette "get_counterparty" do
+      fake_company_id = "4af839ee-b35b-4976-b904-148afc492099"
+      {:ok, body} = ModernTreasurer.get_counterparty(fake_company_id)
+      response = JSON.decode!(body.body)
+      assert response["name"] =~ "CaptainMarvel"
+      assert response["object"] =~ "counterparty"
+    end
   end
 end
